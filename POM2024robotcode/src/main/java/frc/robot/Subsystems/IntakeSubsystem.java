6bdea05f6e7c6c6fabe37f9843dfd02b9f2851e0
Yoav John(Eragon) <yojobama@gmail.com>
2024-01-27 16:04:42 +0200

    adding the intakeRollerSubsystem to the IntakeSubsystem

--- POM2024robotcode/src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
+++ POM2024robotcode/src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
@@ -6,11 +6,14 @@ import com.revrobotics.ColorSensorV3;
 
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Subsystems.intake_subsystems.IntakeRollerSubsystem;
 
 public class IntakeSubsystem extends SubsystemBase 
 {
     // Color Sensor
+    //------------------------------------------------------------------------------------
     public I2C.Port i2cPort =  I2C.Port.kOnboard;
     public  ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
 
@@ -18,6 +21,11 @@ public class IntakeSubsystem extends SubsystemBase
     ColorMatchResult match;
     final Color noteColor = new Color(130, 98, 26);
     final Color blueColor = new Color(54, 113, 86);
+    //------------------------------------------------------------------------------------
+    //the subsystems composing this subsystem
+
+    public IntakeRollerSubsystem rollerSubsystem = new IntakeRollerSubsystem();
+
 
     public IntakeSubsystem()
     {
@@ -29,5 +37,5 @@ public class IntakeSubsystem extends SubsystemBase
     {
         match = m_colorMatcher.matchClosestColor(colorSensor.getColor());
         return match.color == noteColor;
-    }
+    } 
 }
