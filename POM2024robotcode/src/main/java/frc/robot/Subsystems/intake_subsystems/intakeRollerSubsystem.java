87ccbb869d86c945100c6ca2acbb6c04836eee0c
Yoav John(Eragon) <yojobama@gmail.com>
2024-01-27 16:05:11 +0200

    adding comments to intakeRollerSubsystem

--- POM2024robotcode/src/main/java/frc/robot/Subsystems/intake_subsystems/intakeRollerSubsystem.java
+++ POM2024robotcode/src/main/java/frc/robot/Subsystems/intake_subsystems/intakeRollerSubsystem.java
@@ -6,17 +6,19 @@ import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Subsystems.PomSubsystem;
 
 
-public class intakeRollerSubsystem extends PomSubsystem
+public class IntakeRollerSubsystem extends PomSubsystem
 {
     CANSparkMax rollerMotor = new CANSparkMax(0, MotorType.kBrushless);
     private final RelativeEncoder rollerMotorEncoder = rollerMotor.getEncoder();
     private final SparkPIDController rollerMotorPidController = rollerMotor.getPIDController();
 
-    public intakeRollerSubsystem()
+    public IntakeRollerSubsystem()
     {
         // setting the PID controllers properties
         double kI = 0,kP = 0,kD = 0,kFF = 0;
@@ -25,7 +27,7 @@ public class intakeRollerSubsystem extends PomSubsystem
         rollerMotorPidController.setD(kD);
         rollerMotorPidController.setFF(kFF);
     }
-
+    // the subsystems functions
     @Override
     public void stopMotor()
     {
@@ -55,4 +57,7 @@ public class intakeRollerSubsystem extends PomSubsystem
     {
         rollerMotorPidController.setReference(target, ControlType.kPosition);
     }
+
+    // the subsystems commands
+    // public Command OuttakeNoteCommand = Commands.startEnd(() -> this.setMotor(-1), () -> this.stopMotor(), this).until(() -> );
 }
