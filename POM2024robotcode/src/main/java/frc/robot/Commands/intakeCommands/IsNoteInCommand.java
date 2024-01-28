9bedfad9ea8a09b58811dceb55bbc873ee157cd7
Yoav John(Eragon) <yojobama@gmail.com>
2024-01-29 00:36:15 +0200

    updating intakeSubsystem to intakeRollerSubsystem

--- POM2024robotcode/src/main/java/frc/robot/Commands/intakeCommands/IsNoteInCommand.java
+++ POM2024robotcode/src/main/java/frc/robot/Commands/intakeCommands/IsNoteInCommand.java
@@ -4,23 +4,24 @@ import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Subsystems.IntakeSubsystem;
 import frc.robot.Subsystems.LedSubsystem;
+import frc.robot.Subsystems.intake_subsystems.IntakeRollerSubsystem;
 
 public class IsNoteInCommand extends Command
 {
     LedSubsystem ledSubsystem;
-    IntakeSubsystem intakeSubsystem;
+    IntakeRollerSubsystem intakeRollerSubsystem;
 
-    public IsNoteInCommand(LedSubsystem ledSubsystem,IntakeSubsystem intakeSubsystem)
+    public IsNoteInCommand(LedSubsystem ledSubsystem,IntakeRollerSubsystem intakeRollerSubsystem)
     {
         this.ledSubsystem = ledSubsystem;
-        this.intakeSubsystem = intakeSubsystem;
-        addRequirements(intakeSubsystem);
+        this.intakeRollerSubsystem = intakeRollerSubsystem;
+        addRequirements(intakeRollerSubsystem);
     }
 
     @Override
     public void execute()
     {
-        if(intakeSubsystem.isNoteIn()) ledSubsystem.setLeds(Color.kGreen);
+        if(intakeRollerSubsystem.isNoteIn()) ledSubsystem.setLeds(Color.kGreen);
         else ledSubsystem.setLeds(Color.kRed);
     }
 }
