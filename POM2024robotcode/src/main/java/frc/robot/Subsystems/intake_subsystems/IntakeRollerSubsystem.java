4281e83a2912ba3a4a679003b263f77e53009058
Yoav John(Eragon) <yojobama@gmail.com>
2024-01-29 00:02:58 +0200

    changing the one CANSparkMax to two VictorSPX

--- POM2024robotcode/src/main/java/frc/robot/Subsystems/intake_subsystems/IntakeRollerSubsystem.java
+++ POM2024robotcode/src/main/java/frc/robot/Subsystems/intake_subsystems/IntakeRollerSubsystem.java
@@ -1,28 +1,25 @@
 package frc.robot.Subsystems.intake_subsystems;
 
-import com.revrobotics.CANSparkMax;
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkPIDController;
 
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.util.Color;
-
-import com.revrobotics.CANSparkBase.ControlType;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
 import frc.robot.Subsystems.PomSubsystem;
-import static frc.robot.Constants.IntakeConstants.*;
 
 
 public class IntakeRollerSubsystem extends PomSubsystem
 {
-    CANSparkMax rollerMotor = new CANSparkMax(ROLLER_MOTOR, MotorType.kBrushless);
-    private final RelativeEncoder rollerMotorEncoder = rollerMotor.getEncoder();
-    private final SparkPIDController rollerMotorPidController = rollerMotor.getPIDController();
-
+    // Motors
+    //------------------------------------------------------------------------------------
+    VictorSPX rollerMotor = new VictorSPX(10);    
+    VictorSPX rollerMotorSlave = new VictorSPX(11);    
+    //------------------------------------------------------------------------------------
+    
+    
     // Color Sensor
     //------------------------------------------------------------------------------------
     public I2C.Port i2cPort =  I2C.Port.kOnboard;
@@ -36,46 +33,21 @@ public class IntakeRollerSubsystem extends PomSubsystem
 
     public IntakeRollerSubsystem()
     {
-        // setting the PID controllers properties
-        double kI = 0,kP = 0,kD = 0,kFF = 0;
-        rollerMotorPidController.setP(kP);
-        rollerMotorPidController.setI(kI);
-        rollerMotorPidController.setD(kD);
-        rollerMotorPidController.setFF(kFF);
-
-
         m_colorMatcher.addColorMatch(blueColor);
         m_colorMatcher.addColorMatch(noteColor);
+        rollerMotorSlave.follow(rollerMotor);
     }
     // the subsystems functions
     @Override
     public void stopMotor()
     {
-        rollerMotor.set(0);
+        rollerMotor.set(VictorSPXControlMode.PercentOutput,0);
     }
 
     @Override
     public void setMotor(double speed)
     {
-        rollerMotor.set(speed);
-    }
-
-    @Override
-    public void resetEncoder()
-    {
-        rollerMotorEncoder.setPosition(0);
-    }
-
-    @Override
-    public double getEncoderPosition()
-    {
-        return rollerMotorEncoder.getPosition();
-    }
-
-    @Override
-    public void setSetPoint(double target)
-    {
-        rollerMotorPidController.setReference(target, ControlType.kPosition);
+        rollerMotor.set(VictorSPXControlMode.PercentOutput,speed);
     }
 
     public boolean isNoteIn()
