361f83d5afdc4fa1635a262a0607897ae6f3eb0b
uri tabic <tabicuri123@gmail.com>
2024-02-04 23:00:45 +0200

    added set Leds command factory method, and commented unused line

--- POM2024robotcode/src/main/java/frc/robot/Subsystems/LedSubsystem.java
+++ POM2024robotcode/src/main/java/frc/robot/Subsystems/LedSubsystem.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.LedsConstants.*;
 
@@ -48,6 +49,11 @@ public class LedSubsystem extends SubsystemBase {
 
     public void ShuffleboardColor(){
        
+    }
+
+    public Command setLedCommand(Color color)
+    {
+        return this.runOnce(() -> setLeds(color));
     }
 }
 
