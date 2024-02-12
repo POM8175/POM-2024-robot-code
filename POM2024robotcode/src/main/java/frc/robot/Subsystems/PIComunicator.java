0d4f748bb624be725f0d760af0bb7c0496b17f9a
Yoav John(Eragon) <yojobama@gmail.com>
2024-02-12 14:51:35 +0200

    adding the tcp protocol code for PIComunicator

--- POM2024robotcode/src/main/java/frc/robot/Subsystems/PIComunicator.java
+++ POM2024robotcode/src/main/java/frc/robot/Subsystems/PIComunicator.java
@@ -1,21 +1,49 @@
-package frc.robot.Subsystems;
+package frc.robot.Subsystems.RaspberryPi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PIComunicator extends SubsystemBase
 {
+    int port = 10101;
+    ServerSocket serverSocket;
+    Socket piSocket;
+    BufferedReader in;
+    int x = 0,y = 0;
+    OutputStream outputStream;
+
     public PIComunicator()
     {
-        
-    }
-
-    public boolean canRun()
-    {
-        return true;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("server is running on port: " + port);
+            piSocket = serverSocket.accept();
+            System.out.println("client connected on port: " + port);
+            in = new BufferedReader(new InputStreamReader(piSocket.getInputStream()));
+            outputStream = piSocket.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public int[] getNote()
     {
-        return new int[]{};
+        try{
+            outputStream.write(1);
+            outputStream.flush();
+
+            x = Integer.parseInt(in.readLine());
+            y = Integer.parseInt(in.readLine());
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        
+        return new int[]{x,y};
     }
 }
