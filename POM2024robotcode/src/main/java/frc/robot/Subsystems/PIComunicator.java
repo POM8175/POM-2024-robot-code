296c5811b9d3b23add11d883cfbeacc6a6409334
Yoav John(Eragon) <yojobama@gmail.com>
2024-02-16 18:01:52 +0200

    adding the code for PIComunicator

--- POM2024robotcode/src/main/java/frc/robot/Subsystems/PIComunicator.java
+++ POM2024robotcode/src/main/java/frc/robot/Subsystems/PIComunicator.java
@@ -1,59 +1,75 @@
 package frc.robot.Subsystems;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PIComunicator extends SubsystemBase
 {
-    int port = 12253;
-    ServerSocket serverSocket;
-    Socket piSocket;
-    BufferedReader in;
-    int x = 0,y = 0;
-    OutputStream outputStream;
+    HttpRequest request;
+        
+    HttpClient client;
+    HttpResponse<String> response;
+
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode jsonNode;
 
     public PIComunicator()
     {
-        try {
-            serverSocket = new ServerSocket(port);
-            System.out.println("server is running on port: " + port);
-            piSocket = serverSocket.accept();
-            System.out.println("client connected on port: " + port);
-            in = new BufferedReader(new InputStreamReader(piSocket.getInputStream()));
-            outputStream = piSocket.getOutputStream();
-        } catch (IOException e) {
+        try{
+        request = HttpRequest.newBuilder()
+            .uri(new URI("http://pi/getMyBloodyNote"))
+            .GET()
+            .build();
+
+        client = HttpClient.newBuilder()
+            .version(Version.HTTP_1_1)
+            .followRedirects(Redirect.NORMAL)
+            .connectTimeout(Duration.ofSeconds(20))
+            .proxy(ProxySelector.of(new InetSocketAddress("proxy.example.com", 80)))
+            .authenticator(Authenticator.getDefault())
+            .build();
+
+        response = client.send(request, BodyHandlers.ofString());
+        }catch(IOException | URISyntaxException | InterruptedException e){
             e.printStackTrace();
         }
     }
 
     public int[] getNote()
     {
+        int left = 0,right = 0,top = 0,buttom = 0;
         try{
-            outputStream.write(1);
-            outputStream.flush();
-
-            x = Integer.parseInt(in.readLine());
-            y = Integer.parseInt(in.readLine());
-        }catch(IOException e){
+            response = client.send(request, BodyHandlers.ofString());
+            jsonNode = mapper.readTree(response.body());
+            left = jsonNode.get("left").asInt();
+            right = jsonNode.get("right").asInt();
+            top = jsonNode.get("top").asInt();
+            buttom = jsonNode.get("buttom").asInt();
+        }catch(IOException | InterruptedException e){
             e.printStackTrace();
         }
-        
-        return new int[]{x,y};
+
+        return new int[]{left,right,top,buttom};
     }
 
     public void close()
     {
-        try {
-            piSocket.close();
-            serverSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+       
     }
 }
