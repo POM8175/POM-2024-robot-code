fc7f1f051fd163a826792b57f6270c511c73f8d0
odedbe24-meet <106547160+odedbe24-meet@users.noreply.github.com>
2024-02-17 15:29:16 +0200

    Changed transfer motor to neo

--- POM2024robotcode/src/main/java/frc/robot/Subsystems/TransferSubsystem.java
+++ POM2024robotcode/src/main/java/frc/robot/Subsystems/TransferSubsystem.java
@@ -2,12 +2,13 @@ package frc.robot.Subsystems;
 
 import static frc.robot.Constants.TransferConstants.*;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -15,7 +16,7 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class TransferSubsystem extends PomSubsystem
 {
-    VictorSPX transferMotor = new VictorSPX(TRANSFER_MOTOR);
+    CANSparkMax transferMotor = new CANSparkMax(TRANSFER_MOTOR, MotorType.kBrushless);
     // Color Sensor
     //------------------------------------------------------------------------------------
     public I2C.Port i2cPort =  I2C.Port.kOnboard;
@@ -30,7 +31,7 @@ public class TransferSubsystem extends PomSubsystem
         // adding collors to the dataset of m_colorMatcher
         for(int i = 0;i<notNoteColors.length;i++) m_colorMatcher.addColorMatch(notNoteColors[i]);
         m_colorMatcher.addColorMatch(noteColor);
-        transferMotor.setNeutralMode(NeutralMode.Brake);
+        transferMotor.setIdleMode(IdleMode.kBrake);
         setDefaultCommand(this.runOnce(() -> stopMotor()));
     }
     @Override
@@ -47,13 +48,13 @@ public class TransferSubsystem extends PomSubsystem
     @Override
     public void setMotor(double speed)
     {
-        transferMotor.set(ControlMode.PercentOutput, speed);
+        transferMotor.set(speed);
     }
 
     @Override
     public void stopMotor()
     {
-        transferMotor.set(ControlMode.PercentOutput,0);
+        transferMotor.set(0);
     }
 
     // the subsystems commands
