a2debed4fe3e7295e66f5a95fb7800321ccf52c7
uri tabic <tabicuri123@gmail.com>
2024-02-24 21:04:23 +0200

    added shoot move out and added pauses and improved shoot, not tested

--- POM2024robotcode/src/main/java/frc/robot/Commands/autonomousCommands.java
+++ POM2024robotcode/src/main/java/frc/robot/Commands/autonomousCommands.java
@@ -1,6 +1,7 @@
 package frc.robot.Commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Commands.DriveCommands.DriveMeasured;
 import frc.robot.Subsystems.DriveSubsystem;
 import frc.robot.Subsystems.TransferSubsystem;
@@ -10,26 +11,31 @@ import frc.robot.Subsystems.shooting_subsystems.ShootingArmSubsystem;
 import frc.robot.Subsystems.shooting_subsystems.ShootingSubsystem;
 
 public class autonomousCommands {
-    public Command shootMoveOut()
+    public Command shootTryCollect()
     {
-        return arm.OpenForIntakeCommand().
+        return arm.closeSlow().alongWith(shoot.spinWheelsCommand()).
+            andThen(transfer.transfer(true).raceWith(roller.slow(true))).andThen(shoot.stopWheelsCommand()).
+            andThen(arm.OpenForIntakeCommand()).
             andThen(intakeLift.OpenCloseIntake(true)).
-            andThen(arm.closeSlow().alongWith(shoot.spinWheelsCommand())).
-            andThen(transfer.transfer(true)).
-            andThen(shoot.stopWheelsCommand().alongWith(transfer.stopWheelsCommand())).
-            andThen(new DriveMeasured(drive, 0.9).alongWith((roller.intakeNoteCommand()).raceWith(transfer.getFromIntake())));
+            andThen(arm.goToAngleCommand(0).withTimeout(0.4)).
+            andThen(arm.goToAngleCommand(0).alongWith(new DriveMeasured(drive, 0.9)).alongWith((roller.intakeNoteCommand()).raceWith(transfer.getFromIntake())));
     }
     public Command shootCollectShoot()
     {
-        return shootMoveOut().
-        andThen(new DriveMeasured(drive, -0.9).alongWith(shoot.spinWheelsCommand())).
+        return shootTryCollect().
+        andThen(new DriveMeasured(drive, -0.9).alongWith(shoot.spinWheelsCommand())).alongWith(arm.OpenForIntakeCommand().andThen(intakeLift.OpenCloseIntake(false)).andThen(arm.goToAngleCommand(0))).
+        andThen(new WaitCommand(0.17)).
         andThen(transfer.transfer(true)).
         andThen(shoot.stopWheelsCommand().alongWith(transfer.stopWheelsCommand()));        
     }
     public Command shoot()
     {
         return arm.closeSlow().alongWith(shoot.spinWheelsCommand()).
+        andThen(new WaitCommand(0.2)).
         andThen(transfer.transfer(true));
+    }
+    public Command shootMoveOut(){
+        return shoot().andThen(new DriveMeasured(drive, 1.5));
     }
     public autonomousCommands(DriveSubsystem drive, IntakeLiftSubsystem intakeLift, IntakeRollerSubsystem roller, ShootingSubsystem shoot, ShootingArmSubsystem arm, TransferSubsystem transfer)
     {
